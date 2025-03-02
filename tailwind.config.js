852e81e68fee1ab47c0ee02022b87cebcad081f8
Pablo Goulart <pablogoulart02@gmail.com>
2025-03-02 11:38:45 -0300

    estilo pronto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'GrayBlue': 'hsl(212, 23%, 69%)',
+        'LightGrayBlue': 'hsl(210, 46%, 95%)',
+        'DarkGrayBlue': 'hsl(217, 19%, 35%)',
+        'DarkBlue': 'hsl(214, 17%, 51%)',
+      },
+      fontFamily: {
+        Manrope: ['"Manrope"', "Sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
-
